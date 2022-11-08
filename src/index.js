9281fe65f03851489945e42ab8f512108af628d8
Vishvamitra <64136758+vishvamitra4@users.noreply.github.com>
2022-11-09 01:48:17 +0530

    Added All files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+
+alert("zoom in Window for better Experience.")
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
